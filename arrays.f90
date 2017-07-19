b130143a2a3ea36db34e0b0eb83bc814608d6acb
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2017-07-19 09:52:00 +0200

    ARRAYS MODULE ADDED TO REPOSITORY

---
+++ arrays.f90
@@ -0,0 +1,22 @@
+Module arrays
+
+!    Integer :: status1,status2,status3,status4,status5,status6
+!    Integer*4,dimension(13) :: buff
+!    Integer*4,allocatable,dimension(:) :: ID
+
+!!$    Real*8, allocatable, dimension(:) :: PeriodA,HA,Sigma_mA,VA,IIA,old_point
+!!$    Real*8, allocatable, dimension(:) :: PeriodB,HB,Sigma_mB,VB,IIB,current_point
+!!$    Real*8, allocatable, dimension(:) :: PeriodC,HC,Sigma_mC,VC,IIC,mvi5av,Sigma_mvi5av,logP,Mw,sigmaMw
+!!$    Real*8, allocatable, dimension(:) :: Period,H,Sigma_m,V,II,PeriodR11,VIR11,F160WR11,eF160WR11,OHR11
+!!$    Real*8, allocatable, dimension(:) :: Periodm31,Vm31,Hm31,Sigma_m31
+!!$    Real*8, allocatable, dimension(:,:,:,:,:) :: cov,inv_cov
+!!$    Real*4, allocatable, dimension(:) :: acceptance_probability
+!!$    Real*8, allocatable, dimension(:,:) :: Covgauss,Covguess
+!!$    Real*8 :: jumping_factor                           ! SAVES JUMPING FACTOR FOR MCMC (INCREASE IF WANT BIGGER STEP SIZE, DECREASE OTHERWISE) 
+!!$
+!!$    Character(len=10),allocatable,dimension(:) :: Name,NameA,NameB,NameC,Namem31
+!!$    Character(len=5),allocatable,dimension(:) :: Field,Fieldmvi
+  Character(len=7),allocatable,dimension(:) :: PGC
+!!$    Character(len=9),allocatable,dimension(:) :: ID,IDm31
+
+End module arrays
